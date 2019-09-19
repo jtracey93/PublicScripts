8febd3185585d9052b8ef98302b0bdea638e3768
Jack Tracey <jack@jacktracey.co.uk>
2019-09-19 13:07:25 +0100

    script complete and testing started

--- Azure/PowerShell/Partner Tools/LinkPartnerIDAzureAccountsV1.0.ps1
+++ Azure/PowerShell/Partner Tools/LinkPartnerIDAzureAccountsV1.0.ps1
@@ -41,20 +41,20 @@ Write-Host "For the next step you will require the Azure AD Tenant/Directory ID 
 Write-Host ""
 Write-Host "This can be found by following the instructions listed below:" -ForegroundColor Yellow
 Write-Host "1 - Login to the customers Azure portal. `n2 - Select the 'Azure Active Directory' blade/console. `n3 - Select 'Properties' from the left hand side menu. `n4 - The 'Directory ID' is the value you require. Copy this value." -ForegroundColor Magenta
-
-Write-Host "Please enter the customers Azure AD Tenant/Directory ID below:" -ForegroundColor Blue
+Write-Host ""
+Write-Host "Please enter the customers Azure AD Tenant/Directory ID below, followed by pressing the 'Enter/Return' key:" -ForegroundColor Cyan
 $customerAzureADID = Read-Host
 
 # Connect To Azure Account
 
 Write-Host "Logging In To The Azure Management Plane (ARM)..." -ForegroundColor Cyan
 Write-Host ""
-Write-Host "Please login with the account you wish to link your Partner ID." -ForegroundColor Blue
+Write-Host "Please login with the account you wish to link your Partner ID." -ForegroundColor Cyan
 Write-Host "This account must have permissions on the customers Azure platform." -ForegroundColor Yellow
 Write-Host ""
 
 Connect-AzAccount -TenantId $customerAzureADID -ErrorAction SilentlyContinue
-$AzureLoginCheck = Get-AzContext
+$AzureLoginCheck = Get-AzContext -ErrorAction SilentlyContinue
 
 if ($AzureLoginCheck.Tenant.Id -eq $customerAzureADID) {
     Write-Host "Logged in to Azure Management ARM successfully" -ForegroundColor Green
@@ -70,13 +70,13 @@ else {
 
 $MPNPartnerID = $null
 do {
-    Write-Host "Please enter the MPN Partner ID you wish to link this customers Azure too:" -ForegroundColor Blue
+    Write-Host "Please enter the MPN Partner ID you wish to link this customers Azure too:" -ForegroundColor Cyan
     $MPNPartnerID = Read-Host
 } until ($MPNPartnerID -ne "$null")
 
 Write-Host "MPN Partner ID Captured:"$MPNPartnerID -ForegroundColor Green
 
-# Collect Azure Partner ID Info
+# Collect Azure Existing MPN Partner ID Info
 
 Write-Host "Checking if existing MPN Partner ID is set to any value"
 
@@ -85,5 +85,88 @@ $existingMPNPartnerIdInfo = $null
 $existingMPNPartnerIdInfo = Get-AzManagementPartner
 
 if ($existingMPNPartnerIdInfo -eq "$null") {
-    Write-Host "MPN Partner ID Not Set To Any Value" -ForegroundColor Green
+    Write-Host "MPN Partner ID Not Currently Set To Any Value" -ForegroundColor Green
+    Write-Host ""
+}
+else {
+    Write-Host "MPN Partner ID already set to:" $existingMPNPartnerIdInfo.PartnerId -ForegroundColor Yellow
+    Write-Host "The exisiting MPN Partner name is:" $existingMPNPartnerIdInfo.PartnerName -ForegroundColor Yellow
+    Write-Host ""
+}
+
+# Check if Azure MPN Partner IDs Are The Same
+
+if ($existingMPNPartnerIdInfo.PartnerId -eq $MPNPartnerID) {
+    Write-Host "The MPN Partner ID you wish to set is already set as the MPN Partner ID for this customers Azure Tenant" -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "No action is required. This script will close when any key is pressed." -ForegroundColor Green
+    Read-Host
+    exit
+}
+else {
+    Write-Host "The MPN Partner ID you wish to set is different to what is already set." -ForegroundColor Red
+    Write-Host ""
+    Write-Host "The MPN Partner ID you wish to set is:" $MPNPartnerID -ForegroundColor Yellow
+    Write-Host "The MPN Partner ID that is currently set is:" $existingMPNPartnerIdInfo.PartnerId -ForegroundColor Yellow
+    Write-Host "The MPN Partner name that is currently set are:" $existingMPNPartnerIdInfo.PartnerName -ForegroundColor Yellow
+    Write-Host ""
+}
+
+# Check If New Azure MPN Partner ID Should Be Set
+
+$setNewMPNPartnerId = 'n'
+
+Write-Host "Do you wish to set/replace the specified MPN Partner ID for this customers Azure Tenant? `nPlease enter 'y' or 'n' followed by the 'Enter/Return' key. (The default is 'n'):" -ForegroundColor Cyan
+$setNewMPNPartnerId = Read-Host
+
+if ($setNewMPNPartnerId -eq 'n') {
+    Write-Host ""
+    Write-Host "You have chosen not to change/set the MPN Partner ID for this customers Azure Tenant." -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "The MPN Partner ID will remain set as:" $existingMPNPartnerIdInfo.PartnerId -ForegroundColor Cyan
+    Write-Host "Which is for the MPN Partner named:" $existingMPNPartnerIdInfo.PartnerName -ForegroundColor Cyan
+    Write-Host ""
+    Write-Host "This script will close when any key is pressed." -ForegroundColor Red
+    Read-Host ""
+    exit
+}
+
+if ($setNewMPNPartnerId -eq 'y') {
+    Write-Host ""
+    Write-Host "You have chosen to change/set the MPN Partner ID for this customers Azure Tenant to:" $MPNPartnerID -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "Press the 'Enter/Return' key to set the new MPN Partner ID for this customers Azure Tenant." -ForegroundColor Cyan
+    Read-Host
+    Write-Host ""
+    Write-Host "Setting the MPN Parter ID..." -ForegroundColor Cyan
+    New-AzManagementPartner -PartnerId $MPNPartnerID -ErrorAction SilentlyContinue
+    Write-Host ""
+    Write-Host "MPN Partner ID set!" -ForegroundColor Green
+}
+
+# Check if New Azure MPN Partner ID Has Been Set Correctly
+
+Write-Host ""
+Write-Host "Checking MPN Partner ID set correctly..." -ForegroundColor Cyan
+Write-Host ""
+
+$newMPNPartnerIdInfo = $null
+
+$newMPNPartnerIdInfo = Get-AzManagementPartner -ErrorAction SilentlyContinue
+
+if ($newMPNPartnerIdInfo.PartnerId -eq $MPNPartnerID) {
+    Write-Host "MPN Partner ID Set Correctly!" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "Script complete and will therefore close when any key is pressed" -ForegroundColor Green
+    Read-Host
+}
+else {
+    Write-Host "MPN Partner ID Not Set Correctly. Please Check Log File To Investigate" -ForegroundColor Red
+    Write-Host ""
+    Write-Host "MPN Parnter ID currently set to:" $newMPNPartnerIdInfo.PartnerId -ForegroundColor Yellow
+    Write-Host "Which is for the MPN Partner named:" $newMPNPartnerIdInfo.PartnerName
+    Write-Host ""
+    Write-Host "This script will now close when any key is pressed" -ForegroundColor Cyan
+    Read-Host
+    exit
 }
