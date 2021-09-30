682dd258dfb93991c2c4645d9c94c8f3dd863b37
jtracey93 <jack@jacktracey.co.uk>
2021-09-30 15:13:03 +0100

    v1.2 release

--- Azure/PowerShell/Enterprise-scale/Wipe-ESLZAzTenant.ps1
+++ Azure/PowerShell/Enterprise-scale/Wipe-ESLZAzTenant.ps1
@@ -1,8 +1,8 @@
 ######################
 # Wipe-ESLZAzTenant #
 ######################
-# Version: 1.1
-# Last Modified: 29/09/2021
+# Version: 1.2
+# Last Modified: 30/09/2021
 # Author: Jack Tracey 
 # Contributors: Liam F. O'Neill, Paul Grimley, Jeff Mitchell
 
@@ -38,7 +38,13 @@ https://aka.ms/es/guides
 
 # Release notes 29/09/2021 - V1.1:
 - Added checks and break, if not installed, for required Azure PowerShell modules: 'Az' or 'Az.Accounts', 'Az.Resources' & 'Az.ResourceGraph'
+
+# Release notes 30/09/2021 - V1.2:
+- Added checks to ensure this is running on PowerShell Core edition and not Desktop - https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1
+- Added user confirmation prompt with random 8 character code they must enter to confirm before anything is removed/moved by the script
 #>
+
+#Requires -PSEdition Core
 
 [CmdletBinding()]
 param (
@@ -65,13 +71,51 @@ $StopWatch.Start()
 
 # Check required PowerShell modules are installed
 if ((Get-InstalledModule -Name 'Az' -MinimumVersion '6.3.0' -ErrorAction SilentlyContinue) -or ((Get-InstalledModule -Name 'Az.Accounts' -MinimumVersion '2.5.2' -ErrorAction SilentlyContinue) -and (Get-InstalledModule -Name 'Az.Resources' -MinimumVersion '4.3.0' -ErrorAction SilentlyContinue) -and (Get-InstalledModule -Name 'Az.ResourceGraph' -MinimumVersion '0.7.7' -ErrorAction SilentlyContinue))) {
-    Write-Host "Required Az Powershell Modules are installed" -ForegroundColor Green 
-} else {
+    Write-Host "Required Az Powershell Modules are installed" -ForegroundColor Green
+    Write-Host ""
+}
+else {
     throw "Required Az Powershell Modules are installed. Required modules are: 'Az' OR 'Az.Accounts' (v2.5.2+), 'Az.Resources' (v4.3.0+) & 'Az.ResourceGraph' (v0.7.7+)"
 }
 
 # Get all Subscriptions that are in the Intermediate Root Management Group's hierarchy tree
 $intermediateRootGroupChildSubscriptions = Search-AzGraph -Query "resourcecontainers | where type =~ 'microsoft.resources/subscriptions' | mv-expand mgmtGroups=properties.managementGroupAncestorsChain | where mgmtGroups.name =~ '$intermediateRootGroupID' | project subName=name, subID=subscriptionId, subState=properties.state, aadTenantID=tenantId, mgID=mgmtGroups.name, mgDisplayName=mgmtGroups.displayName"
+
+$userConfirmationMGsToDelete = Get-AzManagementGroup -GroupID $intermediateRootGroupID -Expand -Recurse | Select-Object Id, DisplayName, Name, TenantId, ParentId, ParentDisplayName, ParentName, Children
+$userConfirmationSubsToMove = $intermediateRootGroupChildSubscriptions | Select-Object subName, subID, subState, aadTenantID
+
+## Confirm with user that they want to proceed with script removing the hierarchy and resoruces, also prompt them to enter a response to a challenge to confirm
+Write-Host "Before proceeding with wiping of the hierarchy provided as input parameter, please confirm you wish to proceed by removing the below Management Group hierarchy (parent information is shown for assistance in confirming the correct Management Group hierarchy, it will NOT be removed):" -ForegroundColor Cyan
+$userConfirmationMGsToDelete
+
+Write-Host "The above Management Group hierarchy contains the following Subscriptions that will be moved back to the Tenant Root Management Group also as part of this script:" -ForegroundColor Cyan
+Write-Host ""
+if ($null -ne $intermediateRootGroupChildSubscriptions) {
+    $userConfirmationSubsToMove
+} else {
+    Write-Host "No Subscriptions found in selected/entered hierarchy"
+}
+
+# Generate 8 character random string (combination of lowercase letters and integers)
+$userConfirmationRandomID = -join ((48..57) + (97..122) | Get-Random -Count 8 | ForEach-Object { [char]$_ })
+
+Write-Host "To confirm the removal of the above Management Group hierarchy, moving the Subscriptions back to the Tenant Root Management Group, removing all Resoruces and Resource Groups from the Subscriptions and removing all Tenant, seleted/shown Management Groups, and selected/shown Management Groups deployments." -ForegroundColor Yellow
+Write-Host ""
+Write-Host "Please enter the following random string exactly: $userConfirmationRandomID" -ForegroundColor Yellow
+Write-Host ""
+
+Write-Host "Please enter the random string shown above to confirm you wish to contine running this script."
+$userConfirmationInputString = Read-Host -Prompt "(Leave blank or type anything that doesn't match the string above to cancel/terminate)"
+
+if ($userConfirmationInputString -eq $userConfirmationRandomID) {
+    Write-Host ""
+    Write-Host "Confirmation string entered successfully, proceeding to remove hierarchy and resoruces as shown above..." -ForegroundColor Green
+    Write-Host ""
+}
+else {
+    Write-Host "Confirmation string not entered or incorrect, terminating script..." -ForegroundColor Red
+    throw "Confirmation string not entered or incorrectly entered, terminating script..."
+}
 
 Write-Host "Moving all subscriptions under root management group" -ForegroundColor Yellow
 
