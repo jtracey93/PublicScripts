26367976869e6135cedd811eb0cc3715137f7850
Jack Tracey <41163455+jtracey93@users.noreply.github.com>
2023-04-19 11:46:23 +0100

    Update Get-AllBuiltInRBACRolesWithCreadtedAndUpdatedDates.ps1

--- Azure/PowerShell/RBAC/Get-AllBuiltInRBACRolesWithCreadtedAndUpdatedDates.ps1
+++ Azure/PowerShell/RBAC/Get-AllBuiltInRBACRolesWithCreadtedAndUpdatedDates.ps1
@@ -1,3 +1,11 @@
 $allRoles = Invoke-AzRestMethod -Method GET -Path '/providers/Microsoft.Authorization/roleDefinitions?api-version=2022-04-01'
-$tableofRoles = $allRoles.content | ConvertFrom-Json | Select-Object -ExpandProperty value | Select-Object @{Name="roleName";Expression={$_.properties.roleName}}, Name, @{Name="UpdatedOn";Expression={[DateTime]::Parse($_.properties.updatedOn)}}, @{Name="CreatedOn";Expression={[DateTime]::Parse($_.properties.createdOn)}}
-$tableofRoles | Sort-Object -Property UpdatedOn -Descending | Format-Table -AutoSize
+
+$tableOfRoles = $allRoles.content | ConvertFrom-Json | Select-Object -ExpandProperty value | Select-Object @{Name="roleName";Expression={$_.properties.roleName}}, Name, @{Name="Type";Expression={$_.properties.type}}, @{Name="UpdatedOn";Expression={[DateTime]::Parse($_.properties.updatedOn)}}, @{Name="CreatedOn";Expression={[DateTime]::Parse($_.properties.createdOn)}}
+
+Write-Host "Custom Roles:" -ForegroundColor Cyan
+$tableOfRoles | Where-Object {$_.Type -eq "CustomRole"} | Sort-Object -Property UpdatedOn -Descending | Format-Table -AutoSize
+Write-Host ""
+
+Write-Host "Built-In Roles:" -ForegroundColor Cyan
+$tableOfRoles | Where-Object {$_.Type -eq "BuiltInRole"} | Sort-Object -Property UpdatedOn -Descending | Format-Table -AutoSize
+Write-Host ""
